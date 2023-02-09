6da32495d1f399da97fae94a41a0ec521345b40a
lej99 <eunji_ouo@naver.com>
2023-02-09 17:58:32 +0900

    23.02.09(0)

--- js/common.js
+++ js/common.js
@@ -1,3 +1,7 @@
-window.onload = function(){
-    
-}
+$(document).ready(function(){
+    $('.ceo_info_hide').hide();
+    $('.ceo_info>.btn').click(function(){
+        $('.ceo_info_hide').toggle();
+        $(this).find('svg').toggleClass('click');
+    });
+});
