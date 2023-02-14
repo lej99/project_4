d349af8b5d71aa7b9a2b80ee289dc00df6b3deb6
lej99 <eunji_ouo@naver.com>
2023-02-14 17:52:13 +0900

    23.02.14(3)

---
+++ js/sub.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+    var x = setInterval(function () {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        $(".timer").html(hours + "시간 남음");
+        if (distance < 0) {
+            clearInterval(x);
+            $(".timer").html("지났어용");
+        }
+    }, 1000);
+
+});
