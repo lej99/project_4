94f14fada3b59f217823ec6f5d8d4b74c3be8757
lej99 <eunji_ouo@naver.com>
2023-02-15 16:32:29 +0900

    23.02.15(2)

--- js/api_sub.js
+++ js/api_sub.js
@@ -17,15 +17,33 @@ $(document).ready(function(){
         };
     });
 
-    // $.ajax({
-    //     method: "GET",
-    //     url: "https://dapi.kakao.com/v3/search/book?target=title",
-    //     data: { query: "잃어버린 시간을 찾아서 13" },
-    //     headers: { Authorization: "KakaoAK 8313893b989950266cb7d14639361dd5" }
-    // }).done(function (msg) {
+    for (var i=0; i<13; i++){
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: "잃어버린 시간을 찾아서"+(i+1), size:50 },
+            async: false,
+            headers: { Authorization: "KakaoAK 8313893b989950266cb7d14639361dd5" }
+        }).done(function (msg) {
+            $('.book_contents_detail .set .thumbnail').eq(i).attr("src", msg.documents[0].thumbnail,"alt", msg.documents[0].title);
+            $('.book_contents_detail .set .title').eq(i).text("잃어버린 시간을 찾아서 "+(i+1));       
+        });
+    };
 
-    //     $('.info_img img.thumbnail').attr("src",msg.documents[0].thumbnail)
-    //     .attr("alt",msg.documents[0].title)
+    $.get("./text/book_intro.txt", function (data) {
+        $(".book_intro .data").html(data);
+    });
 
-    // });
+    $.get("./text/purblisher_review.txt", function (data) {
+        $(".purblisher_review .data").html(data);
+    });
+
+    $.get("./text/authors_intro.txt", function (data) {
+        $(".authors_intro .data").html(data);
+    });
+
+    $.get("./text/book_list.txt", function (data) {
+        $(".book_list .data").html(data);
+    });
+    
 });
--- js/sub.js
+++ js/sub.js
@@ -12,15 +12,35 @@ $(document).ready(function () {
         }
     }, 1000);
 
-    $('.info_img .img img').click(function(){
+    $('.thumbnail').click(function(){
+        var src = $(this).attr('src')
+        var alt = $(this).attr('alt')
         $('.modal').show();
+        $('.modal img').attr('src',src,'alt',alt)
         $('body').css('overflow','hidden');
     });
     
     $('.modal').hide();
     $('.modal').click(function(){
         $(this).hide();
+        $('.modal img').attr('src','','alt','')
         $('body').css('overflow','visible');
+    });
+
+    $('.book_contents_detail .set .hide').hide();
+    $('.book_contents_detail .set > button').click(function(){
+        $(this).hide();
+        $('.book_contents_detail .set .hide').show();
+    });
+    
+    $('.data').click(function(){
+        if($(this).parent('div').hasClass('plus')){
+            $(this).css({'overflow':'visible','height':'auto'});
+            $(this).parent('div').removeClass('plus');
+        } else {
+            $(this).css({'overflow':'hidden','height':'200px'});
+            $(this).parent('div').addClass('plus');
+        }
     });
 
 });
